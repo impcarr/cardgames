f8a5a7af4bffadb232a29f4d4384397ff56a2a55
Ian <ianmpcarr@gmail.com>
2024-12-27 17:05:12 -0500

    refactor player bidding process

--- high_society/player.py
+++ high_society/player.py
@@ -3,7 +3,7 @@ from typing import List, Optional
 from numpy import exp
 from collections import Counter
 from itertools import combinations
-from auction_card import AuctionCardHand
+from auction_card import AuctionCardHand, AuctionCard
 
 
 class Player:
@@ -28,6 +28,21 @@ class Player:
             self.funds = custom_funds
         self.cards_won = AuctionCardHand()
         self.bid: List[int] = []
+
+    def __repr__(self) -> str:
+        return self.name
+
+    def get_name(self) -> str:
+        """Get the player's name."""
+        return self.name
+
+    def get_funds(self) -> List[int]:
+        """Get the player's money cards."""
+        return self.funds
+
+    def get_cards_won(self) -> AuctionCardHand:
+        """Get the player's auction cards."""
+        return self.cards_won
 
     def get_total_money(self) -> int:
         """Get the total value of all money cards the player has remaining."""
@@ -56,6 +71,13 @@ class Player:
         """Pass on the current bid."""
         self.set_bid([-1])
 
+    def raise_bid(self, raise_amount: List[int]) -> None:
+        raised_bid = self.get_bid() + raise_amount
+        if not self.bid_is_valid(raised_bid):
+            raise ValueError(f"Invalid bid: player does not have requisite cards. Has {self.get_funds()} available, previously bid {self.get_bid()}, and is trying to raise by {raise_amount} for a total raised bid of {raised_bid}")
+        else:
+            self.set_bid(raised_bid)
+
     def bid_is_valid(
         self, bid: list[int], current_highest_bid: Optional[list[int]] = None
     ) -> bool:
@@ -75,35 +97,50 @@ class Player:
                 return False
         return True
 
-    def bid_or_pass_randomly(self, current_bid: List[int]) -> List[int]:
+    def bid_or_pass_randomly(self, current_highest_bid: List[int]) -> List[int]:
         """Randomly pass or return a valid bid based on the current bid."""
-        current_bid_sum = sum(current_bid)
-        if current_bid_sum >= self.get_total_money():
+        if self.get_bid() == [-1]:
+            return self.get_bid()
+        current_highest_bid_sum = sum(current_highest_bid)
+        if current_highest_bid_sum >= self.get_total_money():
             self.pass_bid()
         else:
             if random() < 0.33:
                 self.pass_bid()
             else:
-                self.bid = self.get_random_valid_bid(current_bid)
+                self.raise_bid(self.get_random_valid_raise(current_highest_bid))
         return self.get_bid()
 
-    def get_random_valid_bid(self, current_bid: List[int] = []) -> List[int]:
-        """Uses a nice exponential decay function to chose a bid from the list of valid bids.
+    def get_random_valid_raise(self, current_highest_bid: List[int] = []) -> List[int]:
+        """Uses a nice exponential decay function to choose a bid from the list of valid bids.
         Weights the lower bids more heavily."""
-        valid_bids = self.get_valid_bids(current_bid)
+        valid_bids = self.get_valid_bids(current_highest_bid)
         weights = [exp(-0.5 * i) for i in range(len(valid_bids))]
         total = sum(weights)
         weights = [w / total for w in weights]
         return choices(valid_bids, weights=weights, k=1)[0]
 
-    def get_valid_bids(self, current_bid: List[int]) -> List[List[int]]:
-        """Get all valid bids given the current highest"""
-        valid_bids = []
-        current_bid_sum = sum(current_bid)
+    def get_available_raises(self) -> List[int]:
+        funds_to_raise = [fund for fund in self.get_funds()] # Can't just do a simple 'if fund not in self.get_bid()' because 
+        for fund in self.get_bid():                          # we only want to remove single copies
+            try:
+                funds_to_raise.remove(fund)
+            except ValueError:
+                print(f"Tried to remove {fund} from {funds_to_raise} and failed.")
+        return funds_to_raise
 
-        for length in range(1, len(self.funds) + 1):
-            for combo in combinations(self.funds, length):
-                if sum(combo) > current_bid_sum:
+    def get_valid_bids(self, current_highest_bid: List[int]) -> List[List[int]]:
+        """Get all valid bids given the current highest bid and the player's current bid."""
+        valid_bids = []
+        current_highest_bid_sum = sum(current_highest_bid)
+        most_recent_bid_sum = sum(self.get_bid())
+        raise_minimum = current_highest_bid_sum - most_recent_bid_sum
+
+        funds_available = self.get_available_raises()
+
+        for length in range(1, len(funds_available) + 1):
+            for combo in combinations(funds_available, length):
+                if sum(combo) > raise_minimum:
                     valid_bids.append(list(combo))
 
         return sorted(valid_bids, key=sum)
@@ -114,6 +151,10 @@ class Player:
 
     def spend_bid(self) -> None:
         """Remove current bid from funds and reset current bid to empty."""
-        for card in self.bid:
+        for card in self.get_bid():
             self.funds.remove(card)
         self.reset_bid()
+
+    def win_card(self, card: AuctionCard) -> None:
+        """Add a card to the player's collection."""
+        self.cards_won.win_card(card)
